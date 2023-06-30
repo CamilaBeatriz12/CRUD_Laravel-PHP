226d8bf60235ada24c2843af8720e5db35170bc3
Camila Beatriz da Silva <75540270+CamilaBeatriz12@users.noreply.github.com>
2023-06-30 15:19:31 -0300

    Add files via upload

---
+++ database/migrations/2023_06_28_220034_create_model_prods_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prod', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->double('preco', 10, 2);
+            $table->string('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prod');
+    }
+};
